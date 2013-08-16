6c94a77979446712e3ba03d6090c938343e35b9a
Peter Purnyn <mpur.nyn>
2013-08-16 19:53:29 -0400

    Completed Biweekly4

--- Biweekly4/Section61Group9Biweekly4.m
+++ Biweekly4/Section61Group9Biweekly4.m
@@ -33,7 +33,93 @@ function Section61Group9Biweekly4_OpeningFcn(hObject, eventdata, handles, vararg
     set(handles.togglebutton2,'Enable','off'); 
     handles.alphaFilterOn = false;
     guidata(hObject,handles);
-    closeSerial();
+    %closeSerial();
+    % VARIABLES INITALIZED HERE
+    % it is poor practice to initialize these variables globally when it isn't
+    % neccessary because it takes up memory when they aren't being used.
+    % very very poor programming practice that shouldn't be taught.
+    % I especially hate it because of GUIDE's obtuse way of handeling
+    % global variables.
+    % alas, my grade matters more to me than my priciples.
+    % /rant
+    
+    % this is the maximum length of any array we will be handeling in order
+    % to minimize the amount of memory necessary for our program while
+    % still showing meaningful data on the screen.
+    buf_len = 200;     
+    
+    % create a fixed length empty array for each axis to add data to later
+    % as it comes in from the accellerometer
+    gxdata = zeros(buf_len,1);
+    gydata = zeros(buf_len,1);
+    gzdata = zeros(buf_len,1);
+
+    % creates a fixed length array of ones, to multiply by the threshold
+    % value to create a constant line on the filtered accelerometer graph 
+    % to visually represent the threshold. 
+    thresholdData = ones(buf_len,1);
+
+    % creates a fixed length empty array to hold the tic-toc millisecond
+    % count to properly integrate the velocity graph and to properly label
+    % the axis of the velocity graph.
+    timeData = zeros(buf_len,1);
+
+    % create a fixed length empty array for each axis's filtered data to
+    % add data to later as data comes in from the accelerometer.
+    gxdataFiltered = zeros(buf_len,1);
+    gydataFiltered = zeros(buf_len,1);
+    gzdataFiltered = zeros(buf_len,1);
+    gxVData = gxdata;
+    gyVData = gydata;
+    % this counts the number of  times the threshold line is crossed by the
+    % filtered values of the accelerations of the x, y and z component data
+    % from the accelerometer.
+    xTCount = 0;
+    yTCount = 0;
+    zTCount = 0;
+
+    gx=0;
+    gy=0;
+    gz=0;
+    gxFiltered=0;
+    gyFiltered=0;
+    gzFiltered=0;
+    % creates an index array of the same fixed length to have a reference 
+    % times and a properly labeled axis for the accelorometer data.
+    index = 1:buf_len;
+    
+    alpha = 0; % used for storing alpha filter's alpha value
+    timePeriod = 0; % used for SMA's timeperiod value
+    theta = 0; % used for storing the angle of the accelerometers net acceleration.
+    % lets add these variables to GUIDE's handles object. The ONLY WAY to
+    % have variables accessable globally in GUIDE.
+    handles.index = index;
+    handles.buf_len = buf_len;
+    handles.gxdata = gxdata;
+    handles.gydata = gydata;
+    handles.gzdata = gzdata;
+    handles.thresholdData = thresholdData;
+    handles.timeData = timeData;
+    handles.gxdataFiltered = gxdataFiltered;
+    handles.gydataFiltered = gydataFiltered;
+    handles.gzdataFiltered = gzdataFiltered;
+    handles.xTCount = xTCount;
+    handles.yTCount = yTCount;
+    handles.zTCount = zTCount;   
+    handles.alpha = alpha;
+    handles.timePeriod = timePeriod;
+    handles.theta = theta;
+    handles.gx = gx;
+    handles.gy = gy;
+    handles.gz = gz;
+    handles.gxFiltered = gxFiltered;
+    handles.gyFiltered = gyFiltered;
+    handles.gzFiltered = gzFiltered;
+    handles.gxVData = gxVData;
+    handles.gyVData = gyVData;
+    % Now lets add the handles object to GUIDE's data space so that it can
+    % be accessed by the rest of our program.
+    guidata(hObject,handles);
     
 function varargout = Section61Group9Biweekly4_OutputFcn(hObject, eventdata, handles) 
 varargout{1} = handles.output;
@@ -41,6 +127,7 @@ varargout{1} = handles.output;
 %this function run on clicking the open serial communication toggle button
 function togglebutton1_Callback(hObject, eventdata, handles)
     if(get(hObject,'Value'))
+        
         % if the toggle button is being pressed
         % start opening the serial connection
         
@@ -56,6 +143,7 @@ function togglebutton1_Callback(hObject, eventdata, handles)
         comPort = get(handles.edit1,'String');  
         % this code sets up the serial communication
         % taken from the example code
+        
         if (~exist('serialFlag','var'))
          [accelerometer.s,serialFlag] = setupSerial(comPort);
         end
@@ -91,30 +179,37 @@ function togglebutton2_Callback(hObject, eventdata, handles)
         %were added in another callback function.
         accelerometer = handles.accelerometer;
         calCo = handles.calCo;
+        
+        % now we grab the "global" variables we created in the opening function
+        buf_len = handles.buf_len;
+        gxdata = handles.gxdata;
+        gydata = handles.gydata;
+        gzdata = handles.gzdata;
+        thresholdData = handles.thresholdData;
+        timeData = handles.timeData;
+        gxdataFiltered = handles.gxdataFiltered;
+        gydataFiltered = handles.gydataFiltered;
+        gzdataFiltered = handles.gzdataFiltered;
+        xTCount = handles.xTCount;
+        yTCount = handles.yTCount;
+        zTCount = handles.zTCount;
+        alpha = handles.alpha;
+        timePeriod = handles.timePeriod;
+        theta = handles.theta;
+        gx = handles.gx;
+        gy = handles.gy;
+        gz = handles.gz;
+        gxFiltered = handles.gxFiltered;
+        gyFiltered = handles.gyFiltered;
+        gzFiltered = handles.gzFiltered;
+        gxVData = handles.gxVData;
+        gyVData = handles.gyVData;
+        
         % change the button text to reflect what unpressing it will do
         set(hObject,'String','Stop Reading Accelerometer');
         
-        %the following code is modified from wk3_vector.m & wk3_magnitude.m
-        %sets the length of the arrays
-        buf_len = 200;
-        
-        % create a fixed length empty array for each axis
-        gxdata = zeros(buf_len,1);
-        gydata = zeros(buf_len,1);
-        gzdata = zeros(buf_len,1);
-        thresholdData = ones(buf_len,1);
-        timeData = zeros(buf_len,1);
-        % create a fixed length empty array for each axis's filtere data
-        gxdataFiltered = zeros(buf_len,1);
-        gydataFiltered = zeros(buf_len,1);
-        gzdataFiltered = zeros(buf_len,1);
-        
-        xTCount = 0;
-        yTCount = 0;
-        zTCount = 0;
-        
         %creates an index array of the same fixed length
-        index = 1:buf_len;
+        index = handles.index;
         % Display x and y label ad title.
         xlabel('Time');
         ylabel('Magnitude');
@@ -180,8 +275,7 @@ function togglebutton2_Callback(hObject, eventdata, handles)
             gydata = [gydata(2:end) ; gy];
             gzdata = [gzdata(2:end) ; gz];   
             
-            % thresholdData = get(handles.slider4,'Value')*thresholdData;
-            gmdata = sqrt(gxdata.^2+gydata.^2+gzdata.^2);
+            % gmdata = sqrt(gxdata.^2+gydata.^2+gzdata.^2);
             % if the alpha filter is on
             if(get(handles.togglebutton3,'Value'))
                   %modifies the incoming data with the alpha filter and the
@@ -245,15 +339,22 @@ function togglebutton2_Callback(hObject, eventdata, handles)
             %legend('X','Y','Z','Location','BestOutside'); 
 
            % plot for x y z magnitude
-            axes(handles.axes4); %selects the axes in GUIDE to plot to
-            gxVData = cumtrapz(timeData,gxdata);
-            gyVData = cumtrapz(timeData,gydata);
-            plot(timeData,gxVData,'r', timeData,gyVData,'g');
-            xlim([timeData(1) timeData(end)]);  
-            xlabel('time');
-            ylabel('Velocity');
-            grid on;
-            
+           if(get(handles.togglebutton6,'Value'))
+                axes(handles.axes4); %selects the axes in GUIDE to plot to
+                gxVData = cumtrapz(timeData,gxdata);
+                gyVData = cumtrapz(timeData,gydata);
+                plot(timeData,gxVData,'r', timeData,gyVData,'g');
+                xlim([timeData(1) timeData(end)]);  
+                xlabel('time');
+                ylabel('Velocity');
+                grid on;
+           end
+           axes(handles.axes5);
+           colormap(cool)
+           bar(0:0.1:1,histc(gxdata-gxdataFiltered,0:0.1:1));
+           xlabel('Bins');
+           ylabel('Frequency');
+           grid on;
             %force MATLAB to redraw the figure
             drawnow;
         end
@@ -393,3 +494,20 @@ function slider4_CreateFcn(hObject, eventdata, handles)
 if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor',[.9 .9 .9]);
 end
+
+
+% --- Executes on button press in togglebutton6.
+function togglebutton6_Callback(hObject, eventdata, handles)
+% hObject    handle to togglebutton6 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of togglebutton6
+    if(get(hObject,'Value'))
+        % if the button is being pressed, start reading from the acc.
+        guidata(hObject,handles);
+        set(hObject,'String','Integration Off');
+    else
+        guidata(hObject,handles);
+        set(hObject,'String','Integration On');
+    end
